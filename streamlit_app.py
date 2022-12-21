1204d879bdffe348e861914fd398ecadbab7dbe4
supriyameka <121148742+supriyameka@users.noreply.github.com>
2022-12-20 21:50:31 -0800

    Update streamlit_app.py
    added special smoothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,11 @@
 import streamlit
 
 streamlit.title('My parents new Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favourites')
 streamlit.text('🥣🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥬Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚🥚Hard Boiled Free-Range Egg')
 streamlit.text('🍞🥑Avocado Toast')
+
+streamlit.header('🍐🍌Build your own Fruit Smoothie🥝🍓')
 
