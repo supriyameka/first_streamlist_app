4836ea15e7a710709974f3888038bcd11e8c463c
supriyameka <121148742+supriyameka@users.noreply.github.com>
2022-12-20 21:47:48 -0800

    Update streamlit_app.py
    Added emoji to menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit
 
 streamlit.title('My parents new Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard Boiled Free-Range Egg')
+streamlit.text('🥣🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥬Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚🥚Hard Boiled Free-Range Egg')
+streamlit.text('🍞🥑Avocado Toast')
 
